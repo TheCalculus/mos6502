f632b13811a8787f019905b081cdb5f7a802aacb
TheCalculus <thecalculus@users.noreply.github.com>
2023-08-06 12:16:38 +1000

    fix incorrect references to cpu struct members

--- mos 6502/main.c
+++ mos 6502/main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MEM_SIZE 0x64000  // bytes
 #define ZERO_PG  0x0000   // 0x0000 - 0x00FF
 #define STACK_PG 0x0100   // 0x0100 - 0x01FF
-#define SPEED    1
 
 struct mos6502 {
     uint8_t  A;     // accumulator
@@ -18,41 +18,48 @@ struct mos6502 {
 
     uint8_t* stack;
     uint8_t  memory[MEM_SIZE];
+
+    // EMULATOR FLAGS
+    bool     paused;
+    uint8_t  speed;
 };
 
 struct mos6502* cpu;
 
 // ADDRESSING MODES (BBB)
 
+// TODO: figure out whether all addressing modes (even from different groups) \
+   even need to be in different enums
+
 enum ADDR_MODE_G1 {
     // CC 01, "group one" instructions
-    ZERO_PAGE_X_I = 0b000,  // (zero page, X)
-    ZERO_PAGE     = 0b001,  // zero page
-    IMMEDIATE     = 0b010,  // #immediate
-    ABSOLUTE      = 0b011,  // absolute
-    ZERO_PAGE_Y_I = 0b100,  // (zero page), Y
-    ZERO_PAGE_X   = 0b101,  // zero page, X
-    ABSOLUTE_Y    = 0b110,  // absolute, Y
-    ABSOLUTE_X    = 0b111,  // absolute, X
+    G1_ZERO_PAGE_X_I = 0b000,  // (zero page, X)
+    G1_ZERO_PAGE     = 0b001,  // zero page
+    G1_IMMEDIATE     = 0b010,  // #immediate
+    G1_ABSOLUTE      = 0b011,  // absolute
+    G1_ZERO_PAGE_Y_I = 0b100,  // (zero page), Y
+    G1_ZERO_PAGE_X   = 0b101,  // zero page, X
+    G1_ABSOLUTE_Y    = 0b110,  // absolute, Y
+    G1_ABSOLUTE_X    = 0b111,  // absolute, X
 };
 
 enum ADDR_MODE_G2 {
     // CC 10, "group two" instructions
-    IMMEDIATE     = 0b000,  // #immediate
-    ZERO_PAGE     = 0b001,  // zero page
-    ACCUMULATOR   = 0b010,  // accumulator
-    ABSOLUTE      = 0b011,  // absolute
-    ZERO_PAGE_X   = 0b101,  // zero page, X
-    ABSOLUTE_X    = 0b111,  // absolute, X
+    G2_IMMEDIATE     = 0b000,  // #immediate
+    G2_ZERO_PAGE     = 0b001,  // zero page
+    G2_ACCUMULATOR   = 0b010,  // accumulator
+    G2_ABSOLUTE      = 0b011,  // absolute
+    G2_ZERO_PAGE_X   = 0b101,  // zero page, X
+    G2_ABSOLUTE_X    = 0b111,  // absolute, X
 };
 
 enum ADDR_MODE_G3 {
     // CC 00, "group three" instructions
-    IMMEDIATE     = 0b000,  // #immediate
-    ZERO_PAGE     = 0b001,  // zero page
-    ABSOLUTE      = 0b011,  // absolute
-    ZERO_PAGE_X   = 0b101,  // zero page, X
-    ABSOLUTE_X    = 0b111,  // absolute, X
+    G3_IMMEDIATE     = 0b000,  // #immediate
+    G3_ZERO_PAGE     = 0b001,  // zero page
+    G3_ABSOLUTE      = 0b011,  // absolute
+    G3_ZERO_PAGE_X   = 0b101,  // zero page, X
+    G3_ABSOLUTE_X    = 0b111,  // absolute, X
 };
 
 void initialiseCPU(struct mos6502* cpu) {
@@ -60,7 +67,7 @@ void initialiseCPU(struct mos6502* cpu) {
     cpu->PC = 0x0000;
 
     memset(cpu->memory, 0, sizeof(cpu->memory));
-    stack = &memory[STACK_PG];
+    cpu->stack = &cpu->memory[STACK_PG];
 }
 
 void executeInstruction(uint8_t opcode) {
@@ -72,7 +79,7 @@ void executeInstruction(uint8_t opcode) {
 }
 
 void cycle() {
-    for (int i = 0; i < SPEED; i++) {
+    for (int i = 0; i < cpu->speed; i++) {
         if (cpu->paused)
             continue;
 
